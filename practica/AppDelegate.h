32ef78806d985722419ff11c4b980e3952f88caa
Jose Manuel Franco <manezgz@hotmail.com>
2015-03-29 17:16:15 +0200

    Initial Commit

---
+++ practica/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  practica
+//
+//  Created by Jose Manuel Franco on 29/3/15.
+//  Copyright (c) 2015 Jose Manuel Franco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
